499f988b7ab916da99c02fedc7a79ad34df4c8f4
Henrique Mazzuchetti <henrique.mazzu@gmail.com>
2023-01-06 20:12:00 -0300

    commit fix

--- portfolio-angular/src/app/app.component.ts
+++ portfolio-angular/src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  ngOnInit(): void {
+    console.log(this.today);
+  }
+
   title = 'portfolio-angular';
+  today = new Date().getFullYear();
+  myAge : number = (this.today - 1997);
 }
